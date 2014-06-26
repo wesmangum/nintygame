e470054bb1b88be5c2b821957744ecd3342ddc64
wesmangum <wesmangum24@gmail.com>
2014-06-26 17:52:15 -0500

    top ten only

--- app/models/leaderboard.js
+++ app/models/leaderboard.js
@@ -4,7 +4,7 @@ var leaderboardCollection = global.nss.db.collection('leaderboards');
 
 class Leaderboard{
 	static findByCharacter(obj, fn){
-		leaderboardCollection.find({'character': obj.character}).sort({score: -1}).toArray((err, scores)=>{fn(scores);});
+		leaderboardCollection.find({'character': obj.character}).sort({score: -1}).limit(10).toArray((err, scores)=>{fn(scores);});
 	}
 
 	static submitScore(obj, fn){
